731d3c6420afb26f72e48818751e54b36ed0d069
Thanhcyberpunk <cyberpunklucyt@gmail.com>
2024-12-21 10:13:26 +0700

    gamevr

---
+++ Hand.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+
+namespace SniperDemo
+{
+    public class Hand : UnityEngine.XR.Interaction.Toolkit.Interactors.XRDirectInteractor
+    {
+
+        public GameObject LHand;
+        public GameObject RHand;
+
+        private SkinnedMeshRenderer meshRenderer = null;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            meshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
+        }
+
+        public void SetVisibility(bool value)
+        {
+            //LHand.SetActive(value);
+            //RHand.SetActive(value);
+
+            // meshRenderer.enabled = value;
+        }
+    }
+}
---
+++ Projectile222.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+namespace SniperDemo
+{
+    public class Projectile222 : MonoBehaviour
+    {
+        public int force = 200;
+        public float lifetime = 4;
+
+
+        public void Launch()
+        {
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+            rigidbody.AddRelativeForce(Vector3.forward * force, ForceMode.Impulse);
+            Destroy(gameObject, lifetime);
+        }
+    }
+}
